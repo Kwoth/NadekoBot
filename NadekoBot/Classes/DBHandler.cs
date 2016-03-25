3e602ed994e34178def8db928c3ac541f97e1051
appelemac <appelemac@gmail.com>
2016-03-25 13:37:50 +0100

    small

--- NadekoBot/Classes/DBHandler.cs
+++ NadekoBot/Classes/DBHandler.cs
@@ -23,6 +23,7 @@ namespace NadekoBot.Classes {
                 conn.CreateTable<CurrencyTransaction>();
                 conn.CreateTable<Donator>();
                 conn.CreateTable<UserQuote>();
+                conn.CreateTable<PokeMoves>();
                 conn.CreateTable<PokeTypes>();
                 conn.Execute(Queries.TransactionTriggerQuery);
             }
