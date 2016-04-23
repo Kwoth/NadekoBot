1c60775f6929d8aa88348409aa0c269337194165
appelemac <appelemac@gmail.com>
2016-04-23 18:22:22 +0200

    Pokemon Rework + Feeds
    Full credit for feeds to https://github.com/RogueException/DiscordBot -
    I just reworked it a bit to work on NadekoBot

---
+++ NadekoBot/Modules/Pokemon/TrainerStats.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NadekoBot.Modules.Pokemon
+{
+    class TrainerStats
+    {
+
+        public static int MaxMoves { get; } = 5;
+        /// <summary>
+        /// Amount of moves made since last time attacked
+        /// </summary>
+        public int MovesMade { get; set; } = 0;
+        /// <summary>
+        /// Last people attacked
+        /// </summary>
+        public List<ulong> LastAttacked { get; set; } = new List<ulong>();
+
+    }
+}
---
+++ NadekoBot/_Models/DataModels/PokemonSprite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NadekoBot.DataModels
+{
+    class PokemonSprite : IDataModel
+    {
+        public long OwnerId { get; set; }
+        public string NickName { get; set; }
+        public int HP { get; set; }
+        public long XP { get; set; }
+        public int Level { get; set; }
+        public int SpeciesId { get; set; }
+        public bool IsActive { get; set; }
+            
+        //stats
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int MaxHP { get; set; }
+        public int Speed { get; set; }
+        public int SpecialAttack { get; set; }
+        public int SpecialDefense { get; set; }
+
+
+    }
+}
