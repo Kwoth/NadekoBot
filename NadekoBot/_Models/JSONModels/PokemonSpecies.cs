84ab75e4c40b744138f4d41616396ab7013c91c5
appelemac <appelemac@gmail.com>
2016-04-23 19:39:11 +0200

    Create PokemonSpecies.cs

---
+++ NadekoBot/_Models/JSONModels/PokemonSpecies.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+
+namespace NadekoBot.JSONModels
+{
+    class PokemonSpecies
+    {
+        /// <summary>
+        /// Number in the index
+        /// </summary>
+        public int number { get; set; }
+        /// <summary>
+        /// The name
+        /// </summary>
+        public string name { get; set; }
+
+        public int baseExperience { get; set; }
+
+        /// <summary>
+        /// Contains following keys:
+        /// attack,
+        /// defense,
+        /// special attack,
+        /// special defense,
+        /// hp,
+        /// speed
+        /// </summary>
+        public Dictionary<string, int> baseStats { get; set; }
+
+        /// <summary>
+        /// Level at which it evolves
+        /// </summary>
+        public int evolveLevel { get; set; }
+        
+        /// <summary>
+        /// Pokemon it evolves to
+        /// </summary>
+        public string evolveTo { get; set; }
+        /// <summary>
+        /// Pokemon Types
+        /// </summary>
+        public string[] types { get; set; }
+        /// <summary>
+        /// The set of moves it has; 4 random ones taken from the API
+        /// </summary>
+        public Dictionary<string, string> moves { get; set; }
+        /// <summary>
+        /// link to the image
+        /// </summary>
+        public string imageLink { get; set; }
+    }
+}
