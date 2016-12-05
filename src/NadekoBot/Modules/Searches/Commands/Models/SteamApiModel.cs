0a7b8412b65d79d9fbad0eaf0a2c7a31912690e8
fkndean <admin@darkmatter.zone>
2016-12-05 11:57:40 -0500

    Add steam api model

---
+++ src/NadekoBot/Modules/Searches/Commands/Models/SteamApiModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace NadekoBot.Modules.Searches.Models
+{
+    public class SteamApiModel
+    {
+        //Search User
+        public SteamQueryUser Response { get; set; }
+        public class SteamQueryUser
+        {
+            public string steamid { get; set; } = "";
+            public string message { get; set; } = "";
+            public int success { get; set; }
+
+            public SteamPlayers[] Players { get; set; }
+            public class SteamPlayers
+            {
+                public string steamid { get; set; }
+                public int communityvisibilitystate { get; set; }
+                public int profilestate { get; set; }
+                public string personaname { get; set; }
+                public ulong lastlogoff { get; set; }
+                public string profileurl { get; set; }
+                public string avatar { get; set; }
+                public string avatarmedium { get; set; }
+                public string avatarfull { get; set; }
+                public int personastate { get; set; }
+                public string primaryclanid { get; set; }
+                public ulong timecreated { get; set; }
+                public int personastateflags { get; set; }
+                public string loccountrycode { get; set; }
+            }
+
+        }
+        public enum profilestate
+        {
+            Offline = 0,
+            Online = 1,
+            Busy = 2,
+            Away = 3,
+            Snooze = 4,
+            Private = 0
+        }
+    }
+}
