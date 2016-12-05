e9bf7a55a3969a77f634e5fef301146ead0e18eb
samvaio <samdivaio@icloud.com>
2016-12-06 01:08:19 +0530

    Update OverwatchCommands.cs

--- src/NadekoBot/Modules/Searches/Commands/OverwatchCommands.cs
+++ src/NadekoBot/Modules/Searches/Commands/OverwatchCommands.cs
@@ -44,12 +44,14 @@ namespace NadekoBot.Modules.Searches
                         var lootbox = await http.GetStringAsync($"https://api.lootbox.eu/pc/{region.ToLower()}/{battletag}/profile");
                         var model = JsonConvert.DeserializeObject<OverwatchApiModel>(lootbox);
                         
+                        var rankimg = $"{model.Data.Competitive.rank_img}"
+                        if (rankimg != "https://cdn.discordapp.com/attachments/155726317222887425/255417077383430145/Competpoints_noborders.png")
                         var embed = new EmbedBuilder()
                             .WithAuthor(eau => eau.WithName($"{model.Data.username}")
                             .WithUrl($"https://www.overbuff.com/players/pc/{battletag}")
                             //.WithIconUrl(NadekoBot.Client.GetCurrentUser().AvatarUrl))
                             .WithIconUrl($"{model.Data.avatar}"))
-                            .WithThumbnail(th => th.WithUrl($"{model.Data.Competitive.rank_img}"))
+                            .WithThumbnail(th => th.WithUrl(rankimg))
                             //.AddField(fb => fb.WithName("**Username**").WithValue($"**{model.Data.username}**").WithIsInline(false))
                             .AddField(fb => fb.WithName("**Level**").WithValue($"{model.Data.level}").WithIsInline(true))
                             .AddField(fb => fb.WithName("**Quick Wins**").WithValue($"{model.Data.Games.Quick.wins}").WithIsInline(true))
