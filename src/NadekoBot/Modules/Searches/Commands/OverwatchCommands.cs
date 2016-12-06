9bfe136612f5e88259b5454dc37c66d931b89487
samvaio <samdivaio@icloud.com>
2016-12-06 22:51:37 +0530

    cleanup

--- src/NadekoBot/Modules/Searches/Commands/OverwatchCommands.cs
+++ src/NadekoBot/Modules/Searches/Commands/OverwatchCommands.cs
@@ -21,22 +21,15 @@ namespace NadekoBot.Modules.Searches
         public class OverwatchCommands
         {
             private Logger _log;
-
             public OverwatchCommands()
             {
                 _log = LogManager.GetCurrentClassLogger();
             }
-
-            //Your command
             [NadekoCommand, Usage, Description, Aliases]
             [RequireContext(ContextType.Guild)]
             public async Task Overwatch(IUserMessage umsg, string region, [Remainder] string query = null)
             {
                 var channel = (ITextChannel)umsg.Channel;
-                
-                
-                
-
                 if (string.IsNullOrWhiteSpace(query))
                     return;
                 var battletag = Regex.Replace(query, "#", "-", RegexOptions.IgnoreCase);
@@ -64,8 +57,6 @@ namespace NadekoBot.Modules.Searches
                                 .AddField(fb => fb.WithName("**Competitive Rank**").WithValue("0").WithIsInline(true))
                                 .AddField(fb => fb.WithName("**Competitive Playtime**").WithValue($"{model.Data.Playtime.competitive}").WithIsInline(true))
                                 .AddField(fb => fb.WithName("**Quick Playtime**").WithValue($"{model.Data.Playtime.quick}").WithIsInline(true))
-                                //.WithFooter(fb => fb.WithIconUrl("https://cdn.discordapp.com/attachments/155726317222887425/255653487512256512/YZ4w2ey.png").WithText("Overwatch Stats"))
-                                //.WithImage(tn => tn.Url = "https://cdn.discordapp.com/attachments/155726317222887425/255692313597050880/blank.png")
                                 .WithColor(NadekoBot.OkColor);
                             await channel.EmbedAsync(embed.Build()).ConfigureAwait(false);
                         }
@@ -74,10 +65,8 @@ namespace NadekoBot.Modules.Searches
                             var embed = new EmbedBuilder()
                                 .WithAuthor(eau => eau.WithName($"{model.Data.username}")
                                 .WithUrl($"https://www.overbuff.com/players/pc/{battletag}")
-                            //.WithIconUrl(NadekoBot.Client.GetCurrentUser().AvatarUrl))
                                 .WithIconUrl($"{model.Data.avatar}"))
                                 .WithThumbnail(th => th.WithUrl(rankimg))
-                            //.AddField(fb => fb.WithName("**Username**").WithValue($"**{model.Data.username}**").WithIsInline(false))
                                 .AddField(fb => fb.WithName("**Level**").WithValue($"{model.Data.level}").WithIsInline(true))
                                 .AddField(fb => fb.WithName("**Quick Wins**").WithValue($"{model.Data.Games.Quick.wins}").WithIsInline(true))
                                 .AddField(fb => fb.WithName("**Current Competitive Wins**").WithValue($"{model.Data.Games.Competitive.wins}").WithIsInline(true))
@@ -86,27 +75,14 @@ namespace NadekoBot.Modules.Searches
                                 .AddField(fb => fb.WithName("**Competitive Rank**").WithValue(rank).WithIsInline(true))
                                 .AddField(fb => fb.WithName("**Competitive Playtime**").WithValue($"{model.Data.Playtime.competitive}").WithIsInline(true))
                                 .AddField(fb => fb.WithName("**Quick Playtime**").WithValue($"{model.Data.Playtime.quick}").WithIsInline(true))
-                                //.WithImage(tn => tn.Url = "https://cdn.discordapp.com/attachments/155726317222887425/255692313597050880/blank.png")
-                                //.WithFooter(fb => fb.WithIconUrl("https://cdn.discordapp.com/attachments/155726317222887425/255653487512256512/YZ4w2ey.png").WithText("Overwatch Stats"))
-                                //.WithTimestamp(DateTime.Now)
-                                // was a bad idea -samvaio
                                 .WithColor(NadekoBot.OkColor);
                             await channel.EmbedAsync(embed.Build()).ConfigureAwait(false);
                             return;
                         }
-                        ///await channel.SendMessageAsync($@"Username: {model.Data.username}
-///Level: {model.Data.level}
-///Quick Wins: {model.Data.Games.Quick.wins}
-///Current Competitive Wins: {model.Data.Games.Competitive.wins}
-///Current Competitive Loses: {model.Data.Games.Competitive.lost}
-///Competitive Playtime: {model.Data.Playtime.competitive}
-///Competitive Rank: {model.Data.Competitive.rank}").ConfigureAwait(false);
-
                     }
                 }
                 catch
                 {
-                    //await channel.SendMessageAsync("❎ Found no user with that **BattleTag**.").ConfigureAwait(false);
                     var embed = new EmbedBuilder()
                         .AddField(fb => fb.WithName("**Found no user!**").WithValue("Please check the **Region** and **BattleTag** before trying again.").WithIsInline(true))
                         .WithColor(NadekoBot.ErrorColor);
