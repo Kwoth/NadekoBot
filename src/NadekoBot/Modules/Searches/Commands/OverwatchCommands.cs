e9407cf1654f2a3e032505b3f02651dcd656dd24
samvaio <samdivaio@icloud.com>
2016-12-06 19:09:18 +0530

    Update OverwatchCommands.cs

--- src/NadekoBot/Modules/Searches/Commands/OverwatchCommands.cs
+++ src/NadekoBot/Modules/Searches/Commands/OverwatchCommands.cs
@@ -60,14 +60,18 @@ namespace NadekoBot.Modules.Searches
                                 .AddField(fb => fb.WithName("**Quick Wins**").WithValue($"{model.Data.Games.Quick.wins}").WithIsInline(true))
                                 .AddField(fb => fb.WithName("**Current Competitive Wins**").WithValue($"{model.Data.Games.Competitive.wins}").WithIsInline(true))
                                 .AddField(fb => fb.WithName("**Current Competitive Loses**").WithValue($"{model.Data.Games.Competitive.lost}").WithIsInline(true))
-                                .AddField(fb => fb.WithName("**Competitive Playtime**").WithValue($"{model.Data.Playtime.competitive}").WithIsInline(true))
+                                .AddField(fb => fb.WithName("**Current Competitive Played**").WithValue($"{model.Data.Games.Competitive.played}").WithIsInline(true))
                                 .AddField(fb => fb.WithName("**Competitive Rank**").WithValue("0").WithIsInline(true))
-                                .WithFooter(fb => fb.WithIconUrl("https://cdn.discordapp.com/attachments/155726317222887425/255653487512256512/YZ4w2ey.png").WithText($"Overwatch Stats | Quick Playtime: {model.Data.Playtime.quick}"))
+                                .AddField(fb => fb.WithName("**Quick Playtime**").WithValue($"{model.Data.Playtime.quick}").WithIsInline(true))
+                                .AddField(fb => fb.WithName("**Competitive Playtime**").WithValue($"{model.Data.Playtime.competitive}").WithIsInline(true))
+                                .WithFooter(fb => fb.WithIconUrl("https://cdn.discordapp.com/attachments/155726317222887425/255653487512256512/YZ4w2ey.png").WithText("Overwatch Stats"))
                                 .WithColor(0xf99e1a);
                             await channel.EmbedAsync(embed.Build()).ConfigureAwait(false);
                         }
                         else
                         {
+                            await channel.SendMessageAsync($@"Please Wait...").ConfigureAwait(false)
+                            await channel.TriggerTypingAsync().ConfigureAwait(false)
                             var embed = new EmbedBuilder()
                                 .WithAuthor(eau => eau.WithName($"{model.Data.username}")
                                 .WithUrl($"https://www.overbuff.com/players/pc/{battletag}")
@@ -79,8 +83,10 @@ namespace NadekoBot.Modules.Searches
                                 .AddField(fb => fb.WithName("**Quick Wins**").WithValue($"{model.Data.Games.Quick.wins}").WithIsInline(true))
                                 .AddField(fb => fb.WithName("**Current Competitive Wins**").WithValue($"{model.Data.Games.Competitive.wins}").WithIsInline(true))
                                 .AddField(fb => fb.WithName("**Current Competitive Loses**").WithValue($"{model.Data.Games.Competitive.lost}").WithIsInline(true))
-                                .AddField(fb => fb.WithName("**Competitive Playtime**").WithValue($"{model.Data.Playtime.competitive}").WithIsInline(true))
+                                .AddField(fb => fb.WithName("**Current Competitive Played**").WithValue($"{model.Data.Games.Competitive.played}").WithIsInline(true))
                                 .AddField(fb => fb.WithName("**Competitive Rank**").WithValue(rank).WithIsInline(true))
+                                .AddField(fb => fb.WithName("**Quick Playtime**").WithValue($"{model.Data.Playtime.quick}").WithIsInline(true))
+                                .AddField(fb => fb.WithName("**Competitive Playtime**").WithValue($"{model.Data.Playtime.competitive}").WithIsInline(true))
                                 //.WithImage(tn => tn.Url = $"{model.Data.levelFrame}")
                                 .WithFooter(fb => fb.WithIconUrl("https://cdn.discordapp.com/attachments/155726317222887425/255653487512256512/YZ4w2ey.png").WithText($"Overwatch Stats | Quick Playtime: {model.Data.Playtime.quick}"))
                                 //.WithTimestamp(DateTime.Now)
