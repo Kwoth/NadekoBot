429fce485866b5988b537f8c86f8ded17ae35ee3
Flying Katsu <Warped2713@users.noreply.github.com>
2017-12-09 14:38:22 -0500

    Added unblocked models to GPservice; placeholder condition in TryBlockLate

--- NadekoBot.Core/Modules/Permissions/Services/GlobalPermissionService.cs
+++ NadekoBot.Core/Modules/Permissions/Services/GlobalPermissionService.cs
@@ -13,10 +13,16 @@ namespace NadekoBot.Modules.Permissions.Services
         public readonly ConcurrentHashSet<string> BlockedModules;
         public readonly ConcurrentHashSet<string> BlockedCommands;
 
+        public readonly ConcurrentHashSet<string> UnblockedModules;
+
+        public readonly ConcurrentHashSet<string> UnblockedCommands;
+
         public GlobalPermissionService(IBotConfigProvider bc)
         {
             BlockedModules = new ConcurrentHashSet<string>(bc.BotConfig.BlockedModules.Select(x => x.Name));
             BlockedCommands = new ConcurrentHashSet<string>(bc.BotConfig.BlockedCommands.Select(x => x.Name));
+            UnblockedModules = new ConcurrentHashSet<string>(bc.BotConfig.UnblockedModules.Select(x => x.Name));
+            UnblockedCommands = new ConcurrentHashSet<string>(bc.BotConfig.UnblockedCommands.Select(x => x.Name));
         }
 
         public async Task<bool> TryBlockLate(DiscordSocketClient client, IUserMessage msg, IGuild guild, IMessageChannel channel, IUser user, string moduleName, string commandName)
@@ -28,7 +34,14 @@ namespace NadekoBot.Modules.Permissions.Services
                 (BlockedCommands.Contains(commandName) ||
                 BlockedModules.Contains(moduleName.ToLowerInvariant())))
             {
-                return true;
+                // Check for unblocked command/module
+                if (UnblockedCommands.Contains(commandName) || UnblockedModules.Contains(moduleName.ToLowerInvariant())) 
+                {
+                    // TODO: Check if user/channel/guild matches the associated whitelist item
+                    return false; 
+                }
+                else { return true; }
+                //return true;
                 //return new ExecuteCommandResult(cmd, null, SearchResult.FromError(CommandError.Exception, $"Command or module is blocked globally by the bot owner."));
             }
             return false;
