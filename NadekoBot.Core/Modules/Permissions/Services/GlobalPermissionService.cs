03b1f97936ef0cda225902d9b9e3a5e83398f60a
Flying Katsu <Warped2713@users.noreply.github.com>
2018-02-17 23:35:06 -0500

    globalunblock now allows ubcmd through blocked mdl, and ubmdl through blocked cmd

--- NadekoBot.Core/Modules/Permissions/Services/GlobalPermissionService.cs
+++ NadekoBot.Core/Modules/Permissions/Services/GlobalPermissionService.cs
@@ -2,9 +2,11 @@
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using NadekoBot.Extensions;
 using NadekoBot.Common.Collections;
 using NadekoBot.Common.ModuleBehaviors;
 using NadekoBot.Core.Services;
+using NadekoBot.Core.Services.Impl;
 using NadekoBot.Core.Services.Database.Models;
 
 namespace NadekoBot.Modules.Permissions.Services
@@ -17,56 +19,95 @@ namespace NadekoBot.Modules.Permissions.Services
         public readonly ConcurrentHashSet<string> UnblockedModules;
         public readonly ConcurrentHashSet<string> UnblockedCommands;
         private GlobalWhitelistService _gwl;
+		private NadekoStrings _strs;
 
-        public GlobalPermissionService(IBotConfigProvider bc, GlobalWhitelistService gwl)
+        public GlobalPermissionService(IBotConfigProvider bc, GlobalWhitelistService gwl, NadekoStrings strings)
         {
             BlockedModules = new ConcurrentHashSet<string>(bc.BotConfig.BlockedModules.Select(x => x.Name));
             BlockedCommands = new ConcurrentHashSet<string>(bc.BotConfig.BlockedCommands.Select(x => x.Name));
             UnblockedModules = new ConcurrentHashSet<string>(bc.BotConfig.UnblockedModules.Select(x => x.Name));
             UnblockedCommands = new ConcurrentHashSet<string>(bc.BotConfig.UnblockedCommands.Select(x => x.Name));
             _gwl = gwl;
+			_strs = strings;
         }
 
         public async Task<bool> TryBlockLate(DiscordSocketClient client, IUserMessage msg, IGuild guild, IMessageChannel channel, IUser user, string moduleName, string commandName)
         {
             await Task.Yield();
             commandName = commandName.ToLowerInvariant();
+			moduleName = moduleName.ToLowerInvariant();
 
-            if (commandName != "resetglobalperms" &&
-                (BlockedCommands.Contains(commandName) ||
-                BlockedModules.Contains(moduleName.ToLowerInvariant())))
-            {
-                // System.Console.WriteLine("FOUND A BLOCKED CMDorMDL");
+			if (commandName != "resetglobalperms") {
 
-                // Check for unblocked command/module
-                if (UnblockedCommands.Contains(commandName))
-                {
-                    // System.Console.WriteLine("Detected unblocked cmd {0}", commandName);
-					// Return false if command is unblocked for user OR channel OR server
-                    return !(_gwl.CheckIfUnblocked(commandName, UnblockedType.Command, 
-								user.Id, GlobalWhitelistType.User)
-							|| _gwl.CheckIfUnblocked(commandName, UnblockedType.Command, 
-								channel.Id, GlobalWhitelistType.Channel)
-							|| _gwl.CheckIfUnblocked(commandName, UnblockedType.Command, 
-								guild.Id, GlobalWhitelistType.Server)); 
-                }
-                else if (UnblockedModules.Contains(moduleName.ToLowerInvariant())) 
-                {
-                    // System.Console.WriteLine("Detected unblocked mdl {0}", moduleName);
-                    string modName = moduleName.ToLowerInvariant();
-					// Return false if module is unblocked for user OR channel OR server
-					return !(_gwl.CheckIfUnblocked(modName, UnblockedType.Module, 
-								user.Id, GlobalWhitelistType.User)
-							|| _gwl.CheckIfUnblocked(modName, UnblockedType.Module, 
-								channel.Id, GlobalWhitelistType.Channel)
-							|| _gwl.CheckIfUnblocked(modName, UnblockedType.Module, 
-								guild.Id, GlobalWhitelistType.Server)); 
-                }
-                else { return true; }
+				// If module is blocked, check if either module or command is unblocked
+				if (BlockedModules.Contains(moduleName)) 
+				{
+					bool mdlIsUB = UnblockedModules.Contains(moduleName) &&
+						IsUnblocked(moduleName, UnblockedType.Module, user.Id, channel.Id, guild.Id);
+
+					if (mdlIsUB) {
+						// Pass!
+						return false;
+					} else {
+						// Check if an unblocked command overrides blocked module!
+						bool cmdIsUB = UnblockedCommands.Contains(commandName) &&
+							IsUnblocked(commandName, UnblockedType.Command, user.Id, channel.Id, guild.Id);
+
+						if (cmdIsUB) {
+							// Pass!
+							return false;
+						} else {
+							// Block it!
+							await ReportBlockedCmdOrMdl(channel, guild.Id, UnblockedType.Module, moduleName);
+							return true;
+						}
+					}
+				}
+
+				// If command is blocked, but not the module
+				else if (BlockedCommands.Contains(commandName))
+				{
+					bool cmdIsUB = UnblockedCommands.Contains(commandName) &&
+						IsUnblocked(commandName, UnblockedType.Command, user.Id, channel.Id, guild.Id);
+					
+					if (cmdIsUB) {
+						// Pass!
+						return false;
+					} else {
+						// Check if an unblocked module overrides blocked command!
+						bool mdlIsUB = UnblockedModules.Contains(moduleName) &&
+							IsUnblocked(moduleName, UnblockedType.Module, user.Id, channel.Id, guild.Id);
+
+						if (mdlIsUB) {
+							// Pass!
+							return false;
+						} else { 
+							// Block it!
+							await ReportBlockedCmdOrMdl(channel, guild.Id, UnblockedType.Command, commandName);
+							return true;
+						}
+					}
+				}
+				else { return false; }
                 //return true;
                 //return new ExecuteCommandResult(cmd, null, SearchResult.FromError(CommandError.Exception, $"Command or module is blocked globally by the bot owner."));
             }
             return false;
         }
+
+		private bool IsUnblocked(string name, UnblockedType type, ulong uid, ulong cid, ulong gid)
+		{
+			return (_gwl.CheckIfUnblocked(name, type, uid, GlobalWhitelistType.User)
+					|| _gwl.CheckIfUnblocked(name, type, cid, GlobalWhitelistType.Channel)
+					|| _gwl.CheckIfUnblocked(name, type, gid, GlobalWhitelistType.Server));
+		}
+
+		private async Task ReportBlockedCmdOrMdl(IMessageChannel channel, ulong gid, UnblockedType type, string name)
+		{
+			await channel.SendErrorAsync(
+				_strs.GetText("blocker_embed_title", gid, "permissions"),
+				_strs.GetText("blocker_embed_desc", gid, "permissions", Format.Code(type.ToString()), Format.Bold(name))
+			);
+		}
     }
 }
